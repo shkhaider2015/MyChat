4a6866c0074e897f87e6134ed76e1cb13d3e4e1f
Haider <shkhaider2015@gmail.com>
2020-03-02 18:44:12 +0500

    ddasd

--- app/src/main/java/com/example/mychat/SiginActivity.java
+++ app/src/main/java/com/example/mychat/SiginActivity.java
@@ -3,12 +3,28 @@ package com.example.mychat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SiginActivity extends AppCompatActivity {
+
+    private static final String TAG = "SiginActivity";
+    private EditText mEmail, mPassword;
+    private Button mSigninButton;
+    private TextView mNotHaveAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sigin);
+    }
+
+    private void init()
+    {
+        mEmail = findViewById(R.id.signin_username);
+        mPassword = findViewById(R.id.signin_password);
+        mSigninButton = findViewById(R.id.signin_button);
+        mNotHaveAccount = findViewById(R.id.signin_Dont_have_account);
     }
 }
--- app/src/main/java/com/example/mychat/SignUpActivity.java
+++ app/src/main/java/com/example/mychat/SignUpActivity.java
@@ -1,14 +1,68 @@
 package com.example.mychat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
+    private EditText mFullname, mEmail, mPassword, mConfirmpassword;
+    private RadioGroup mRadioGroup;
+    private RadioButton mMale, mFemale;
+    private Button mSignUp;
+    private TextView mHaveAccount;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        init();
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void init()
+    {
+        mFullname = findViewById(R.id.sign_up_username);
+        mEmail = findViewById(R.id.sign_up_email);
+        mPassword = findViewById(R.id.sign_up_password);
+        mConfirmpassword = findViewById(R.id.sign_up_confirm_password);
+        mRadioGroup = findViewById(R.id.sign_up_radio_group);
+        mSignUp = findViewById(R.id.sign_up_button);
+        mHaveAccount = findViewById(R.id.sign_up_have_account);
+    }
+
+    private void uploadInformation(String email, String password)
+    {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            Log.d(TAG, "onComplete: Create user with email and password : success");
+                        }
+                        else
+                        {
+                            Log.w(TAG, "onComplete: Failure ", task.getException());
+                        }
+                    }
+                });
     }
 }
