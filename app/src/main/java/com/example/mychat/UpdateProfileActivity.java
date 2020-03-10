8b166034981cb3495bea1e2e679c1092ae5badc0
Haider <shkhaider2015@gmail.com>
2020-03-10 10:19:24 +0500

    ddasd

--- app/src/main/java/com/example/mychat/UpdateProfileActivity.java
+++ app/src/main/java/com/example/mychat/UpdateProfileActivity.java
@@ -1,8 +1,15 @@
 package com.example.mychat;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +21,16 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class UpdateProfileActivity extends AppCompatActivity {
 
     private static final String TAG = "UpdateProfileActivity";
+    private static final int GALLERY_REQUEST_CODE = 101;
+    private static final int CAMERA_REQUEST_CODE = 102;
+
+
     private EditText mFullName, mEmail, mPassword, mPhoneNumber;
     private ImageView mImageView;
     private RadioGroup mRadioGroup;
@@ -25,8 +39,11 @@ public class UpdateProfileActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     DatabaseReference mDatabaseRef;
 
+    private Bitmap imageBitmap = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
         init();
@@ -68,6 +85,89 @@ public class UpdateProfileActivity extends AppCompatActivity {
         {
             Log.e(TAG, "loadInformation: ", e);
         }
+
+    }
+
+    private void showPictureDialog()
+    {
+        AlertDialog.Builder picDialog = new AlertDialog.Builder(this);
+        picDialog.setTitle("Select Action");
+        String[] picDialogItems = {"Select From Galley", "Select From Camera"};
+
+        picDialog.setItems(picDialogItems, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                switch (which)
+                {
+                    case 0:
+                        //choose photo from gallery
+                        chooseImageFromGallery();
+                        break;
+                    case 1:
+                        // choose image from camera
+                        takeImageFromCamera();
+                        break;
+                }
+            }
+        });
+
+        picDialog.show();
+
+
+    }
+
+    private void chooseImageFromGallery()
+    {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
+    }
+
+    private void takeImageFromCamera()
+    {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == this.RESULT_CANCELED)
+            return;
+
+        if (requestCode == GALLERY_REQUEST_CODE)
+        {
+
+            if (data != null)
+            {
+                Uri contentUri = data.getData();
+                try
+                {
+                    Bitmap thumbnail = ImageUtility.getResizedBitmap(ImageUtility.getImage(ImageUtility.getBytes(getContentResolver().openInputStream(contentUri))), 200);
+                    imageBitmap = thumbnail;
+                    // imageView.setImageBitmap(thumbnail);
+
+                }catch (FileNotFoundException e)
+                {
+                    Log.e(TAG, "onActivityResult: ", e);
+                }catch (IOException e)
+                {
+                    Log.e(TAG, "onActivityResult: ", e);
+                }
+            }
+        }
+        else if (requestCode == CAMERA_REQUEST_CODE)
+        {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            imageBitmap = ImageUtility.getResizedBitmap(thumbnail ,200);
+            //ImageView.setImageBitmap(thumbnail);
+        }
+        else
+        {
+            return;
+        }
+
 
     }
 }
